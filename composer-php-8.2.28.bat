46c6280a0bb64aec248ee15c7df85f3511bebcd6
Kendall Sawh <53149812+kendallsawh@users.noreply.github.com>
2025-10-29 14:39:01 -0400

    composer and gitbash launch bat files

---
+++ composer-php-8.2.28.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Laravel\php\php-8.2.28\php.exe C:\Laravel\tools\composer.phar %*
---
+++ launch_gitbash_php-8.2.28.bat
@@ -0,0 +1,5 @@
+@echo off
+set PATH=C:\Laravel\php\php-8.2.28;%PATH%
+echo PHP version being used:
+C:\Laravel\php\php-8.2.28\php.exe -v
+"C:\Program Files\Git\bin\bash.exe" --login -i
